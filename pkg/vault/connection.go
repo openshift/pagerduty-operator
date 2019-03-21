180f77c2c91a807452617942a0a36eef76d6a3ae
Wesley Hearn <whearn@redhat.com>
2019-03-21 13:17:35 -0400

    Save vault output to a temp file so we don't hit vault each run

--- pkg/vault/connection.go
+++ pkg/vault/connection.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
 
 	"github.com/hashicorp/vault/api"
 	corev1 "k8s.io/api/core/v1"
@@ -14,6 +17,66 @@ import (
 )
 
 var log = logf.Log.WithName("pagerduty_vault")
+
+func queryVault(vaultURL string, vaultToken string, vaultMount string, vaultPath string, vaultProperty string) (string, error) {
+	vaultFullPath := fmt.Sprintf("%v/data/%v", string(vaultMount), vaultPath)
+
+	client, err := api.NewClient(&api.Config{
+		Address: string(vaultURL),
+	})
+	if err != nil {
+		return "", err
+	}
+	client.SetToken(string(vaultToken))
+
+	vault, err := client.Logical().Read(vaultFullPath)
+	if err != nil {
+		return "", err
+	}
+
+	secret, ok := vault.Data["data"].(map[string]interface{})
+	if !ok {
+		return "", errors.New("Error parsing secret data")
+	}
+
+	if len(vault.Warnings) > 0 {
+		for i := len(vault.Warnings) - 1; i >= 0; i-- {
+			log.Info(vault.Warnings[i])
+		}
+	}
+
+	if len(vault.Data) == 0 {
+		return "", errors.New("Vault data is empty")
+	}
+
+	for propName, propValue := range secret {
+		if propName == vaultProperty {
+			value := fmt.Sprintf("%v", propValue)
+			if len(value) <= 0 {
+				return "", errors.New(vaultProperty + " is empty")
+			}
+			return value, nil
+		}
+	}
+
+	return "", errors.New(vaultProperty + " not set in vault")
+}
+
+func saveSecret(path string, value string) error {
+	os.Remove(path)
+	file, err := os.Create(path)
+	if err != nil {
+		log.Error(err, "Failed to create temp file")
+		return err
+	}
+	_, err = file.WriteString(value)
+	if err != nil {
+		log.Error(err, "Failed to write to temp file")
+		return err
+	}
+
+	return nil
+}
 
 // GetVaultSecret Gets a designed token from vault. Vault creds are stored in a k8s secret
 func GetVaultSecret(osc client.Client, namespace string, secretname string, path string, property string) (string, error) {
@@ -47,47 +110,27 @@ func GetVaultSecret(osc client.Client, namespace string, secretname string, path
 	if len(vaultMount) <= 0 {
 		return "", errors.New("VAULT_MOUNT is empty")
 	}
-	fmt.Println(string(vaultMount))
 
-	client, err := api.NewClient(&api.Config{
-		Address: string(vaultURL),
-	})
-	if err != nil {
-		return "", err
-	}
-	client.SetToken(string(vaultToken))
-
-	vaultPath := fmt.Sprintf("%v/data/%v", string(vaultMount), path)
-
-	vault, err := client.Logical().Read(vaultPath)
-	if err != nil {
-		return "", err
-	}
-
-	secret, ok := vault.Data["data"].(map[string]interface{})
-	if !ok {
-		return "", errors.New("Error parsing secret data")
-	}
-
-	if len(vault.Warnings) > 0 {
-		for i := len(vault.Warnings) - 1; i >= 0; i-- {
-			log.Info(vault.Warnings[i])
+	tempFilePath := fmt.Sprintf("/tmp/%v-%v", string(vaultMount), path)
+	tempFile, err := os.Stat(tempFilePath)
+	if os.IsNotExist(err) || tempFile.ModTime().Before(time.Now().Add(time.Hour*time.Duration(-6))) {
+		secret, err := queryVault(string(vaultURL), string(vaultToken), string(vaultMount), path, property)
+		if err != nil {
+			return "", err
+		}
+		err = saveSecret(tempFilePath, secret)
+		if err != nil {
+			log.Error(err, "Failed to save secret")
+			return secret, nil
 		}
 	}
 
-	if len(vault.Data) == 0 {
-		return "", errors.New("Vault data is empty")
+	fileDat, err := ioutil.ReadFile(tempFilePath)
+	if err != nil {
+		log.Error(err, "Failed to read file - removing")
+		os.Remove(tempFilePath)
+		return queryVault(string(vaultURL), string(vaultToken), string(vaultMount), path, property)
 	}
 
-	for propName, propValue := range secret {
-		if propName == property {
-			value := fmt.Sprintf("%v", propValue)
-			if len(value) <= 0 {
-				return "", errors.New(property + " is empty")
-			}
-			return value, nil
-		}
-	}
-
-	return "", errors.New(property + " not set in vault")
+	return string(fileDat), nil
 }
