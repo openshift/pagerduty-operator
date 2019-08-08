f4b7501ac5d3e852e91b4689154b6dc782e8cb89
Liam Pieri <lpieri@redhat.com>
2019-08-08 14:48:11 -0400

    Update API metrics to summary instead of guage

--- pkg/localmetrics/localmetrics.go
+++ pkg/localmetrics/localmetrics.go
@@ -15,6 +15,7 @@
 package localmetrics
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
@@ -31,19 +32,22 @@ const (
 
 var (
 	MetricPagerDutyCreateFailure = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "metric_pageduty_create_failure",
-		Help: "Metric for the failure of creating a cluster deployment.",
-	}, []string{"name", "clusterdeployment_name"})
+		Name:        "pagerduty_create_failure",
+		Help:        "Metric for the failure of creating a cluster deployment.",
+		ConstLabels: prometheus.Labels{"name": "pagerduty-operator"},
+	}, []string{"clusterdeployment_name"})
 
 	MetricPagerDutyDeleteFailure = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "metric_pageduty_delete_failure",
-		Help: "Metric for the failure of deleting a cluster deployment.",
-	}, []string{"name", "clusterdeployment_name"})
+		Name:        "pagerduty_delete_failure",
+		Help:        "Metric for the failure of deleting a cluster deployment.",
+		ConstLabels: prometheus.Labels{"name": "pagerduty-operator"},
+	}, []string{"clusterdeployment_name"})
 
-	MetricPagerDutyHeartbeat = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "metric_pageduty_heartbeat",
-		Help: "Metric for heartbeating of the Pager Duty api.",
-	}, []string{"name"})
+	MetricPagerDutyHeartbeat = prometheus.NewSummary(prometheus.SummaryOpts{
+		Name:        "pagerduty_heartbeat",
+		Help:        "Metric for heartbeating of the Pager Duty api.",
+		ConstLabels: prometheus.Labels{"name": "pagerduty-operator"},
+	})
 
 	MetricsList = []prometheus.Collector{
 		MetricPagerDutyCreateFailure,
@@ -53,10 +57,10 @@ var (
 )
 
 // UpdateAPIMetrics updates all API endpoint metrics ever 5 minutes
-func UpdateAPIMetrics(APIkey string) {
+func UpdateAPIMetrics(APIKey string, timer *prometheus.Timer) {
 	d := time.Tick(5 * time.Minute)
 	for range d {
-		UpdateMetricPagerDutyHeartbeatGauge(APIkey)
+		UpdateMetricPagerDutyHeartbeat(APIKey, timer)
 	}
 
 }
@@ -64,48 +68,46 @@ func UpdateAPIMetrics(APIkey string) {
 // UpdateMetricPagerDutyCreateFailure updates guage to 1 when creation fails
 func UpdateMetricPagerDutyCreateFailure(x int, cd string) {
 	MetricPagerDutyCreateFailure.With(prometheus.Labels{
-		"name": "pagerduty-operator", "clusterdeployment_name": cd}).Set(
+		"clusterdeployment_name": cd}).Set(
 		float64(x))
 }
 
 // UpdateMetricPagerDutyDeleteFailure updates guage to 1 when deletion fails
 func UpdateMetricPagerDutyDeleteFailure(x int, cd string) {
 	MetricPagerDutyDeleteFailure.With(prometheus.Labels{
-		"name": "pagerduty-operator", "clusterdeployment_name": cd}).Set(
+		"clusterdeployment_name": cd}).Set(
 		float64(x))
 }
 
-// UpdateMetricPagerDutyHeartbeatGauge curls the PD API, updates the gauge to 1
+// UpdateMetricPagerDutyHeartbeat curls the PD API, updates the gauge to 1
 // when successful.
-func UpdateMetricPagerDutyHeartbeatGauge(APIkey string) {
+func UpdateMetricPagerDutyHeartbeat(APIKey string, timer *prometheus.Timer) {
 	metricLogger := log.WithValues("Namespace", "pagerduty-operator")
 	metricLogger.Info("Metrics for PD API")
 
 	_, err := http.NewRequest("GET", "api.pagerduty.com", nil)
 	if err != nil {
-		MetricPagerDutyHeartbeat.With(prometheus.Labels{
-			"name": "pagerduty-operator"}).Set(float64(0))
+		MetricPagerDutyHeartbeat.Observe(float64(timer.ObserveDuration()))
 		metricLogger.Error(err, "Failed to get reach api")
 	}
 
-	// if there is an api key make an authenticated calld
-	if APIkey != "" {
+	// if there is an api key make an authenticated called
+	if APIKey != "" {
 		req, _ := http.NewRequest("GET", apiEndpoint, nil)
 		req.Header.Set("Accept", "application/vnd.pagerduty+json;version=2")
 		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Authorization", "Token token="+APIkey)
-
+		req.Header.Set("Authorization", fmt.Sprintf("Token token=%s", APIKey))
 		resp, err := http.DefaultClient.Do(req)
 
 		if err != nil {
 			metricLogger.Error(err, "Failed to reach api when authenticated")
-			MetricPagerDutyHeartbeat.With(prometheus.Labels{
-				"name": "pagerduty-operator"}).Set(float64(0))
+			MetricPagerDutyHeartbeat.Observe(
+				float64(timer.ObserveDuration().Seconds()))
+
 			return
 		}
 		defer resp.Body.Close()
 
 	}
-	MetricPagerDutyHeartbeat.With(prometheus.Labels{
-		"name": "pagerduty-operator"}).Set(float64(1))
+	MetricPagerDutyHeartbeat.Observe(float64(0))
 }
