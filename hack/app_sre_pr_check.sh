78de5c52463ae64d40dc987a206f0230325b2b24
Liam Pieri <lpieri@redhat.com>
2019-09-13 10:28:00 -0400

    add yaml validation

--- hack/app_sre_pr_check.sh
+++ hack/app_sre_pr_check.sh
@@ -4,6 +4,25 @@
 
 set -exv
 
+CURRENT_DIR=$(dirname "$0")
+YAML_DIRS=( "${CURRENT_DIR}/../deploy/crds" "${CURRENT_DIR}/../manifests" )
+
+for DIR in $YAML_DIRS 
+do 
+	if [[ -d $DIR ]]; then
+		python "$CURRENT_DIR"/validate_yaml.py $CRD_DIR
+
+		if [ "$?" != "0" ]; then
+		    exit 1
+		fi
+
+	else
+		echo "WARNING: No yaml for validation in directiory $DIR"
+	fi
+done 
+
+exit 0
+
 BASE_IMG="pagerduty-operator"
 IMG="${BASE_IMG}:latest"
 
