e0df2633f887a7fb2d59f0f4dae4639db6fe246b
Gerard Ryan <gerard@ryan.lt>
2020-07-02 16:55:03 +0100

    Use go:generate directive to generate correct CRD

--- pkg/apis/pagerduty/v1alpha1/zz_generated.deepcopy.go
+++ pkg/apis/pagerduty/v1alpha1/zz_generated.deepcopy.go
@@ -40,7 +40,7 @@ func (in *PagerDutyIntegration) DeepCopyObject() runtime.Object {
 func (in *PagerDutyIntegrationList) DeepCopyInto(out *PagerDutyIntegrationList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]PagerDutyIntegration, len(*in))
